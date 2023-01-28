ed17db832bfee1d823aab206d4789c9c42b7136b
IniciandoElcambio <carlosgctec@mail.com>
2023-01-28 01:08:17 -0400

    aprendiendo modulos en js import y export

---
+++ js/aritmetica.js
@@ -0,0 +1,9 @@
+export function suma(a=2 , b=4)
+{
+  return a+b;
+}
+
+export function resta(a,b)
+{
+    return a-b;
+}
---
+++ js/modulos.js
@@ -0,0 +1,9 @@
+console.log("Archivo emcripts 6");
+
+import {a} from "./constantes.js"; 
+console.log(a);
+
+import {suma,resta} from "./aritmetica.js";
+
+console.log(suma()); 
+//console.log(resta(3,4));
